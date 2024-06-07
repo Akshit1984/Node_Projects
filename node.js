8e64d5c1a2a1173092a057d56682b79f4a4e2a8b
AKSHIT SHARMA <116747473+Akshit1984@users.noreply.github.com>
2024-06-08 02:14:06 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,15 +1,15 @@
 const http=require('http')
-const port =3000
+const PORT =3000
 
 const server =http.createServer(function(req,res){
     res.write("Hello World")
     res.end()
 })
 
-server.listen(port,function(error){
+server.listen(PORT,function(error){
     if (error){
         console.log("SOmething went wrong")
     } else{
-        console.log("Server is listening on port 3000")
+        console.log("Server is listening on port "+ PORT)
     }
 })
